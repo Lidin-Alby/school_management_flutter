78685b89b032f9e88895bd598ea105e1465b3696
Lidin-Alby <lidinalby@gmail.com>
2025-01-13 11:03:15 +0530

    13 jan

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "545991121"
+    serviceWorkerVersion: "3182230107"
   }
 });
